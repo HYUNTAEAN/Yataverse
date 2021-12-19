a3e3402b4ee95107056d262b488e9320529fe2de
HYUNTAEAN <ppapmuzik@gmail.com>
2021-12-19 23:05:09 +0900

    AWS

--- yataverse/src/main/java/com/blog/yataverse/YataverseApplication.java
+++ yataverse/src/main/java/com/blog/yataverse/YataverseApplication.java
@@ -2,9 +2,14 @@ package com.blog.yataverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class YataverseApplication {
+public class YataverseApplication extends SpringBootServletInitializer {
+
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(YataverseApplication.class); }
 
 	public static void main(String[] args) {
 		SpringApplication.run(YataverseApplication.class, args);
